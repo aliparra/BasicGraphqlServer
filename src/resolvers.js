cdef3ef041a5712bfdb1fa3dd02bcbd5057bdcb8
aliparra <alicia@redparra.com>
2021-06-21 17:57:46 +0200

    Create first query and resolvers

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,11 +1,19 @@
 
 //Aquí le decimos qué hacer cuando llegue una consulta
 //Aquí creamos funciones
+//Es similar al controlador en REST
 
 export const resolvers = {
     Query:{
         hello: () => {
             return "Hello World with Graphql"
+        },
+        myAge(){
+            return 25
+        },
+        greet(root, {name} ){
+           console.log(name) 
+           return `Hello ${name}!`
         }
     }
 }
--- src/schema.js
+++ src/schema.js
@@ -3,9 +3,17 @@ import {resolvers} from "./resolvers"
 import buildSchema from "graphql"
 
 //Define qué se puede consultar
+
+//Tipos de graphql (Strings, Int, Float, Boolean, ID)
+//En la sintaxis graphql no uso comas
+//La ! es para que sea requerido
+//Es similar a la ruta en REST
+
 const typeDefs = `
     type Query {
         hello: String
+        myAge: Int
+        greet(name:String!):String
     }
 `
 
